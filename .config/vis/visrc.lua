a4c6b138a0577d0a7c1055e0935e7c59d3b48891
Joshua Haase <hahj87@gmail.com>
2021-11-25 10:21:54 -0600

    Reemplazar tabulador por 4 espacios.

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,6 +1,13 @@
 require('vis')
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command('set numbers on')
-	vis:command('set autoindent on')
-end)
+vis.events.subscribe
+(
+    vis.events.WIN_OPEN,
+
+    function(win)
+	   vis:command('set numbers on')
+	   vis:command('set autoindent on')
+	   vis:command('set tw 4')
+	   vis:command('set et')
+    end
+)
