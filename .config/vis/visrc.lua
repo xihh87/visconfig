127b2d3a4dc2ccd3208affb2c7ca05f6e726c5e7
Joshua Haase <hahj87@gmail.com>
2022-01-12 13:46:22 -0600

    2 width tabs for yaml files.

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,13 +1,27 @@
 require('vis')
 
-vis.events.subscribe
-(
-    vis.events.WIN_OPEN,
+vis.events.subscribe(
+	vis.events.INIT,
+	function()
+		vis:command('set autoindent on')
+		vis:command('set expandtab on')
+		vis:command('set tabwidth 4')
+	end
+)
 
+vis.events.subscribe(
+   vis.events.WIN_OPEN,
+   function()
+      vis:command('set numbers on')
+      vis:command('set colorcolumn 72')
+   end
+)
+
+vis.events.subscribe(
+    vis.events.FILE_OPEN,
     function(win)
-	   vis:command('set numbers on')
-	   vis:command('set autoindent on')
-	   vis:command('set tw 4')
-	   vis:command('set et')
+       if win.syntax ~= 'yaml' then
+          vis:command('set tabwidth 2')
+       end
     end
 )
