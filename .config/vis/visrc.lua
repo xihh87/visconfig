ec7a62656047f82c1104ebec5cdeb7edd07cceee
Joshua Haase <joshua.haase@ibero.mx>
2022-01-13 09:27:00 -0600

    Set visibility settings on each window.

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -4,17 +4,24 @@ vis.events.subscribe(
 	vis.events.INIT,
 	function()
 		vis:command('set autoindent on')
-		vis:command('set expandtab on')
-		vis:command('set tabwidth 4')
 	end
 )
 
 vis.events.subscribe(
-   vis.events.WIN_OPEN,
-   function()
-      vis:command('set numbers on')
-      vis:command('set colorcolumn 72')
-   end
+	vis.events.WIN_OPEN,
+	function(win)
+		vis:command('set numbers on')
+		vis:command('set show-spaces on')
+		vis:command('set show-tabs on')
+		vis:command('set show-newlines on')
+		vis:command('set show-eof on')
+
+		vis:command('set colorcolumn 72')
+
+		vis:command('set tabwidth 4')
+		vis:command('set expandtab on')
+
+	end
 )
 
 vis.events.subscribe(
