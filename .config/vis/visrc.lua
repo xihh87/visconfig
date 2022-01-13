f1d3371f39ee6f07b04ebe8784209e19cf30c00b
Joshua Haase <joshua.haase@ibero.mx>
2022-01-13 09:41:47 -0600

    Spaces are too much clutter.

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -11,7 +11,7 @@ vis.events.subscribe(
 	vis.events.WIN_OPEN,
 	function(win)
 		vis:command('set numbers on')
-		vis:command('set show-spaces on')
+		vis:command('set show-spaces off')
 		vis:command('set show-tabs on')
 		vis:command('set show-newlines on')
 		vis:command('set show-eof on')
